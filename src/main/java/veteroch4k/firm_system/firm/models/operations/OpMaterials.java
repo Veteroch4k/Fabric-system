e8b77d5e20774d0cb05d465d82e71ea9f32038a3
Veteroch4k <indefinite05@mail.ru>
2025-01-12 17:40:25 +0700

    Completed first migration's file

--- src/main/java/veteroch4k/firm_system/firm/models/operations/OpMaterials.java
+++ src/main/java/veteroch4k/firm_system/firm/models/operations/OpMaterials.java
@@ -2,8 +2,12 @@ package veteroch4k.firm_system.firm.models.operations;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
--- src/main/java/veteroch4k/firm_system/firm/models/operations/Operation.java
+++ src/main/java/veteroch4k/firm_system/firm/models/operations/Operation.java
@@ -10,7 +10,6 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import veteroch4k.firm_system.firm.models.factories.Factory;
 
 @NoArgsConstructor
 @AllArgsConstructor
